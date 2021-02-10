43aa09e80643839e97c9b2decf7f72db53a1f68b
andrew-madsen <andrewrobertmadsen@gmail.com>
2021-02-10 07:52:14 -0700

    Added basic front end and more calculations to the back end.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -14,7 +23,11 @@ def read_root():
 def read_income(income: int, q: Optional[str] = None):
     return {"income": income,
         "income class": income_class(income),
-        "income percentile": income_percentile_range(income)
+        "income percentile": income_percentile_range(income),
+        "max_mortgage_payment": max_mortgage_payment(income),
+        "max_debt_payment": max_debt_payment(income),
+        "max_home_price": max_home_price(income),
+        "max_car_payment": max_car_payment(income)
     }
 
 def income_class(income):
@@ -39,3 +52,18 @@ def income_percentile_range(income):
         return "80th < income < 95th"
     elif income < 270002:
         return "> 95th"
+
+def max_mortgage_payment(income):
+    return (income * 0.28) / 12
+
+def max_debt_payment(income):
+    return (income * 0.36) / 12
+
+def max_home_price(income):
+    max_payment = max_mortgage_payment(income)
+    mir = 0.0275 / 12
+    nmp = 12 * 30
+    return max_payment * ((1+mir)**nmp-1) / (mir*(1+mir)**nmp)
+
+def max_car_payment(income):
+    return (income * 0.1) / 12
